2e6170ec9e548fb645197e3af14a09fe7cb18176
seungju-mmc <ksj970213@hanyang.ac.kjr>
2021-11-28 23:13:07 +0900

    .

--- RL_DQN/Player.py
+++ RL_DQN/Player.py
@@ -58,8 +58,8 @@ class Player():
         if end_time is None:
             end_time = STARTDAY
         if USE_GYM:
-            # self.sim = gym.make("PongNoFrameskip-v4")
-            self.sim = gym.make('SpaceInvadersNoFrameskip-v4')
+            self.sim = gym.make("PongNoFrameskip-v4")
+            # self.sim = gym.make('SpaceInvadersNoFrameskip-v4')
         else:
             self.sim = Simulator(to=end_time, duration=DURATION)
 
