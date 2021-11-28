30f5bb39be75b3fdad01a8c22d75f60c97c15d6f
seungju-mmc <ksj970213@hanyang.ac.kjr>
2021-11-29 02:49:53 +0900

    .

--- RL_DQN/Player.py
+++ RL_DQN/Player.py
@@ -324,7 +324,9 @@ class Player():
             max_next_state_value = float(next_state_value[action].cpu().detach().numpy())
             td_error = r + (GAMMA)**UNROLL_STEP * max_next_state_value - current_state_value
             td_error = min(1, max(td_error, -1))
-            return (abs(td_error) + 1e-4) ** ALPHA
+            x = (abs(td_error) + 1e-4) ** ALPHA
+            
+            return x
         
     def gym_run(self):
         obsDeque = deque(maxlen=4)
