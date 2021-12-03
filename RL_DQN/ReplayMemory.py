d1615ac42917b079984064cf5edd0ff82aa1bf4f
seungju-mmc <ksj970213@hanyang.ac.kjr>
2021-12-03 16:41:55 +0900

    .

--- RL_DQN/ReplayMemory.py
+++ RL_DQN/ReplayMemory.py
@@ -259,7 +259,7 @@ class Replay_Server(threading.Thread):
     def sample(self):
         if len(self.deque) > 0:
             with self._lock:
-                # print(len(self.deque))
+                print(len(self.deque))
                 return pickle.loads(self.deque.pop(0))
                 # return self.deque.pop(0)
         else:
