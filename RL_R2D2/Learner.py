e163c3ae17fd9d16d77b0a25b6476893df883ef7
seungju-mmc <ksj970213@hanyang.ac.kjr>
2021-12-09 20:59:16 +0900

    .

--- RL_R2D2/Learner.py
+++ RL_R2D2/Learner.py
@@ -142,7 +142,7 @@ class Learner:
         # td_error_for_prior = (np.abs(td_error_for_prior) + 1e-7) ** ALPHA
         td_error_for_prior = np.reshape(td_error_for_prior, (79, -1))
         
-        new_priority = td_error_for_prior.max(1) * 0.9 + 0.1 * td_error_for_prior.mean(1)
+        new_priority = td_error_for_prior.max(0) * 0.9 + 0.1 * td_error_for_prior.mean(0)
         print(new_priority.shape)
 
         td_error_view = td_error.view(79, -1)
