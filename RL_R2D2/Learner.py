4a2a33f111f1d5bd11be9d4401aa6705b9cfac65
seungju-mmc <ksj970213@hanyang.ac.kjr>
2021-12-09 19:44:53 +0900

    .

--- RL_R2D2/Learner.py
+++ RL_R2D2/Learner.py
@@ -77,6 +77,7 @@ class Learner:
 
         reward= reward.astype(np.float32)
         reward = np.transpose(reward, (1, 0))
+        reward = torch.tensor(reward).float().to(self.device)
 
         action_value = self.model.forward([state, shape])[0].view(-1)
         # 320 * 6
@@ -102,6 +103,7 @@ class Learner:
             target_value = next_max_value[UNROLL_STEP+1:].contiguous()
 
             rewards = np.zeros((80 - UNROLL_STEP - 1, BATCHSIZE))
+            rewards = torch.zeros((80 - UNROLL_STEP -1 , BATCHSIZE)).float().to(self.device)
             bootstrap = next_max_value[-1].detach()
             done = torch.tensor(done).float().to(self.device)
             remainder = [bootstrap * done]
@@ -112,7 +114,7 @@ class Learner:
                     reward[-(i+1)] + GAMMA * remainder[i]
                 )
             
-            rewards = torch.tensor(rewards).float().to(self.device)
+            # rewards = torch.tensor(rewards).float().to(self.device)
             remainder = remainder[::-1]
             remainder.pop()
             # remainder = torch.tensor(remainder).float().to(self.device)
