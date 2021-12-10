984cff451ceea9520f504c422ee517603467a401
seungju-mmc <ksj970213@hanyang.ac.kjr>
2021-12-10 20:29:12 +0900

    .

--- Simulator/simulator_v1.py
+++ Simulator/simulator_v1.py
@@ -319,7 +319,7 @@ class Simulator:
             raise RuntimeWarning("Action Space !!")
         
         if idle:
-            reward = reward_ - 1
+            reward = reward_ - 0.001
         else:
             reward = reward_
         
